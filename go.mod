88247dca89a7b7cccd4bb87f0842001af10c07a7
Thomas Elsgaard <thomas.elsgaard@truecommerce.com>
2022-06-07 08:41:34 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module portTester
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	portPtr := flag.String("port", "80", "Listening port")
+
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+*portPtr)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + *portPtr)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+
+	fmt.Println("Got connection")
+	conn.Close()
+}
